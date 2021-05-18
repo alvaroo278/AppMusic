dfc3106567f35bb55a96565c842f6bb3e107df6f
alvaroo278 <alvaronn99@gmail.com>
2021-05-18 13:06:36 +0200

    DAO

--- AppMusic/src/persistencia/IAdaptadorCancionDAO.java
+++ AppMusic/src/persistencia/IAdaptadorCancionDAO.java
@@ -1,5 +1,14 @@
 package persistencia;
 
+import java.util.List;
+
+import dominio.Cancion;
+
+
 public interface IAdaptadorCancionDAO {
 
+	public void registrarCancion(Cancion cancion);
+	public void borrarCancion(Cancion cancion);
+	public Cancion recuperarCancion(int codigo);
+	public List<Cancion> recuperarTodasCanciones();
 }
