c36d3ef76d5c8f43c1a02f26c47005a3c0cbbbd0
Molina8 <jesuceheginjm@gmail.com>
2021-05-18 18:33:08 +0200

    Revert "fafa"
    This reverts commit 6fa774cd94e6015e1a7a19322e63178535aafc01.

--- AppMusic/src/ventanas/Principal.java
+++ AppMusic/src/ventanas/Principal.java
@@ -114,13 +114,6 @@ public class Principal {
 		panelSuperior.add(btnNewButton_1);
 		
 		JButton btnNewButton = new JButton("Logout");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				frmPrincipal.setVisible(false);
-				Login log = new Login();
-				log.getFrame().setVisible(true);
-			}
-		});
 		panelSuperior.add(btnNewButton);
 		
 		JPanel panelIzq = new JPanel();
